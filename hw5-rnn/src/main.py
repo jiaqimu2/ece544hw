620a5282510c31a3497d1274b8f1f44413230ccf
jiaqimu2 <mujq10@gmail.com>
2016-11-13 11:24:42 -0600

    fix memory issue by downsampling images

--- hw5-rnn/src/main.py
+++ hw5-rnn/src/main.py
@@ -11,8 +11,8 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-FIGURE_PATH = '/Users/mujq10/ece544hw/hw5-rnn/figures/'
-DATA_PATH = '/Users/mujq10/ece544hw/hw5-rnn/data/'
+FIGURE_PATH = '/home/jiaqimu2/ece544hw/hw5-rnn/figures/'
+DATA_PATH = '/home/jiaqimu2/ece544hw/hw5-rnn/data/'
 os.system('mkdir -p %s' % FIGURE_PATH)
 os.system('mkdir -p %s' % DATA_PATH)
 
@@ -24,16 +24,20 @@ def evaluate(true_Label, pred_Label):
 
 def preprocess(data, dimension):
 
-	num_sample, _ = data.shape
+	num_sample, num_dim = data.shape
+	# downsampling
+	if dimension <=2: 
+		data = data.reshape(num_sample, 28, 28)
+		data = data[:, ::2, ::2]
 	return data.reshape((num_sample, -1, dimension))
 
 def classification(train_data, train_label, test_data, test_label, label = 'RNN'):
 	
 	print '-'*50
 	if label == 'RNN':
-		classifier = RNN(epoch_size = 200, hidden_size = 100, batch_size = 128)
+		classifier = RNN(epoch_size = 300, hidden_size = 100, batch_size = 128)
 	else:
-		classifier = LSTM(epoch_size = 200, hidden_size = 100, batch_size = 128)
+		classifier = LSTM(epoch_size = 300, hidden_size = 100, batch_size = 128)
 
 	iter_nums, accuracies = classifier.fit(train_data, train_label)
 	fig_file = FIGURE_PATH + '%s-%d-%d.pdf' % (label, train_data.shape[1], train_data.shape[2])
@@ -57,15 +61,15 @@ if __name__ == '__main__':
 
 	# setting 1 and setting 2 cannot run in one shot!!!!
 	# setting 1
-	train_tmp_data = preprocess(train_data, 28)
-	test_tmp_data = preprocess(test_data, 28)
-
-	classification(train_tmp_data, train_label, test_tmp_data, test_label, 'RNN')
-	classification(train_tmp_data, train_label, test_tmp_data, test_label, 'LSTM')
-
-	# setting 2
-	# train_tmp_data = preprocess(train_data, 1)
-	# test_tmp_data = preprocess(test_data, 1)
+	# train_tmp_data = preprocess(train_data, 28)
+	# test_tmp_data = preprocess(test_data, 28)
 
 	# classification(train_tmp_data, train_label, test_tmp_data, test_label, 'RNN')
 	# classification(train_tmp_data, train_label, test_tmp_data, test_label, 'LSTM')
+
+	# setting 2 / downsampling 
+	train_tmp_data = preprocess(train_data, 1)
+	test_tmp_data = preprocess(test_data, 1)
+
+	classification(train_tmp_data, train_label, test_tmp_data, test_label, 'RNN')
+	classification(train_tmp_data, train_label, test_tmp_data, test_label, 'LSTM')
